c6c492c5688eb071228774ce2187d0fcd20057b5
Olu Isaac <oluisaac7@gmail.com>
2019-03-11 19:23:15 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,13 +2,13 @@ const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
 const path = require('path')
-
+console.error()
 
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
 
-  it('calls console.error("help")', done => {
+  it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
 
     jsdom.env(html, [src], {
